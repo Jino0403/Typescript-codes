99121d5fd319cbf7a7d69ff808fc7660f37575b2
Jino0403 <jinho9203@naver.com>
2024-03-29 01:16:32 +0900

    generics Studying..

--- 08.generics/src/main.ts
+++ 08.generics/src/main.ts
@@ -1,44 +1,164 @@
-class Post {
-  public id: number = 0; // 초기값 설정 가능
-  public title: string = ''; //초기값 설정 가능
+@ 예시 1
 
-  constructor(id: number, title: string) {
-    this.id = id;
-    this.title = title;
-  }
+// Generic 사용 전
+// function getArrayLength(arr: number[] | string[] | boolean[]) {
+//   return arr.length;
+// }
 
-  // 위 두개를 합쳐서 생략하려면 아래와 같이 작성하면 된다. 기본값도 추가 가능하다.
-  // constructor(
-  //   public  id: number = 0,
-  //   public title: string = ''
-  // ) {}
+// const array1 = [1, 2, 3];
+// const array2 = ['a', 'b', 'c'];
+// const array3 = [true, false, true];
 
-  getPost() {
-    return `postId: ${this.id}, postTitle: ${this.title}`;
+// getArrayLength(array1);
+// getArrayLength(array2);
+// getArrayLength(array3);
+
+// getArrayLength에 다른 타입의 배열을 넣어주고 싶을때 마다, getArrayLength의 매개변수의 타입을
+// union타입으로 추가해줄 수도 있지만 Generic을 사용해서 더 깔끔하게 만들 수 있음
+
+// Generic 사용 후
+function getArrayLength<T>(arr: T[]) {
+  return arr.length;
+}
+// 함수 이름 옆에 <T> 라고 넣고 매개변수 타입을 T[]라고 해준 후에
+
+const array1 = [1, 2, 3];
+const array2 = ['a', 'b', 'c'];
+const array3 = [true, false, true];
+
+getArrayLength<number>(array1);
+getArrayLength<string>(array2);
+getArrayLength<boolean>(array3);
+
+// 위와 같이 <T> 자리에 넣고자하는 배열의 타입을 넣으면 된다.
+
+
+@ 예시 2
+
+interface Vehicle {
+  name:string;
+  color:string;
+  option:any;
+}
+
+const car:Vehicle = {
+  name: 'Car',
+  color: 'Red',
+  option: {
+    price: 1000
   }
 }
 
-const post = new Post(1, 'title 1');
+const bike:Vehicle = {
+  name: 'Bike',
+  color: 'Green',
+  option: true
+}
 
-// TypeScript에서는 this로 접근하는 속성들을 위한 type을 class body 안에 넣어줘야한다.
-// 안넣어주면 위의 title과 id에 'Post' 형식에 'title' or 'id' 속성이 없습니다 라고 뜰것
+// 이와 같이 car와 bike 타입 지정까지 해준 상황에서 Vehicle의 option: any에서 any를 쓰지 않고
+// 타입을 처리해 주고 싶음, 이 때도 Generic 사용가능
 
-// access modifier는 세 개가 존재함(접근 제어자)
+interface Vehicle<T> {
+  name:string;
+  color:string;
+  option:T;
+}
 
-// public default 값이며, 어디서나 접근 가능
-// protected  클래스 내, 상속받은 자식 클래스에서 접근 가능
-// private 클래스 내에서만 접근 가능
-
-console.log(post.id);
-console.log(post.title);
-
-// public을 protected로 바꾸면 에러가 뜬다. 클래스내, 상속받은 자식클래스에서 접근 가능하기에
-// 클래스 새로 하나 생성
-
-class PostB extends Post {
-  getPost() {
-    return this.title;
+const car:Vehicle<{price:number}> = {
+  name: 'Car',
+  color: 'Red',
+  option: {
+    price: 1000
   }
 }
 
-//Post 클래스의 하위 클래스이기 때문에 문제없이 사용 가능
+const bike:Vehicle<boolean> = {
+  name: 'Bike',
+  color: 'Green',
+  option: true
+}
+
+// 위와 같이 Vehicle<T> 써주고 제네릭을 사용하고자 하는 부분에 위와같이 사용해주면, 각각에 맞춰 잘 들어감
+
+ @ Generic 사용시 장점 !
+// Generic 사용시 재사용성이 높은 함수와 클래스를 생성할 수 있음
+// any 처럼 타입을 직접 지정하지 않지만, 타입을 체크해 컴파일러가 오류를 찾을 수 있게됨
+
+
+ @ 예시 3 (함수 매개변수 1개 일때)
+ // non-generic
+ const makeArr = (x: number) => {
+  return [x];
+ }
+
+ const arr = makeArr(5);
+ const arr1 = makeArr("a"); // 매개변수 타입이 넘버가 아니므로 오류 발생
+
+  // generic
+ const makeArr1 = <T>(x: T) => {
+    return [x];
+ }
+
+const arr2 = makeArr1(5);
+const arr3 = makeArr1("a");
+
+ // 이와 같이 해주면 문제 없이 됨
+
+  @ 예시 4 (함수 매개변수 2개일 때)
+  const makeArry = <T,Y>(x:T, y:Y) : [T,Y] => {
+    return [x,y];
+  }
+  
+  const arry = makeArry<number , number>(4,5);
+  const arry1 = makeArry<string , string>("a","b"); 
+  
+  // 위와 같이 타입을 지정해 줄 수있고, T,Y의 경우 어느것을 써도 상관은 없음.
+  // 반환하는 타입은 매개변수의 타입인 T와 Y를 배열에 넣어 반환하면 됨
+  // 또한 T,Y에 아래와 같이기본값을 넣어줄 수 있음.
+
+  const makeArry = <T,Y = string>(x:T, y:Y) : [T,Y] => {
+    return [x,y];
+  }
+
+  @ 예시 5 (Generic과 Extends 같이 사용)
+
+  const makeFullName  = (obj: {firstName: string, lastName: string}) => {
+    return {
+      ...obj,
+      fullName: obj.firstName + " " + obj.lastName
+    }
+  }
+
+  makeFullName({firstName:"John", lastName: "Doe", location:"Seoul"})
+  // firstName과 lastName말고 location을 넣어주고 싶은데 object에 타입으로 넣어 줄 때 없었어서 오류 발생
+  // 아래와 같이 해주면 됨
+
+  const makeFullName  = <T extends {firstName: string, lastName: string}> (obj: T) => {
+    return {
+      ...obj,
+      fullName: obj.firstName + " " + obj.lastName
+    }
+  }
+
+  // 이렇게 작성을 하게 되면 obj에 매개변수로 firstName과 lastName부분은 항상 들어오고 그 다음 여기에서 확장해서 
+  // 새로운 것들이 들어올 수 있게 됨 
+
+
+  @ React에서의 Generic
+
+  import React from 'react'
+
+  interface Props {
+    name: string;
+  }
+
+  const ReactComponent:React.FC<Props> = ({name}) => {
+    const [state] = useState<{name: string|null}>({name:""});
+  
+    state.name
+
+    return <div>{name}</div>
+  }
+
+  // 컴포넌트에 Props로 무엇이 올지 모르기 때문에 Generic을 사용하고
+  // useState도 Generic 타입을 받습니다.
