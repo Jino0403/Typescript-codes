f37c8f1bd5ee4a105487541d0905446814abdde3
Jino0403 <jinho9203@naver.com>
2024-03-29 02:59:50 +0900

    keyof Studying..

--- 11. keyof/src/main.ts
+++ 11. keyof/src/main.ts
@@ -1,61 +1,31 @@
-@ Extends 
-Extends 키워드는 자바스크립트에서도 사용할 수 있으며 부모 클래스에 있는 프로퍼티나 메소드를 상속해서 사용할 수 있게 만들어줌
-
-@ implements
-implements 키워드는 새로운 클래스의 타입 체크를 위해서 사용되며,  그 클래스의 모양을 정의할 때 사용합니다.
-(부모 클래스의 프로퍼티와 메소드를 상속받아서 사용하는게 아님)   
-
-class Car {
-   mileage = 0;
-   price = 100;
-   color = 'white';
-
-   drive() {
-    return 'drive!';
-   }
-
-   brake() {
-    return 'brake!';
-   }
+@ 예제 1
+interface IUser {
+  name: string;
+  age: number;
+  address: string;
 }
 
-class Ford extends Car {
+type UserKeys = keyof IUser;
+// 여긴 바로 type으로 되어있기 때문에 keyof 바로 사용
+// "name" | "age" | "address" 이렇게 반환됨
 
+@ 예제 2
+const user = {
+  name: 'John',
+  age: 20,
+  address: 'Seoul',
+};
+// 이 객체를 이용하여 Union 타입 만드려면? user 객체를 Type으로 바꾼다음에 keyof연산자를 적용!
+
+type UserKeys = keyof typeof user;
+// "name" | "age" | "address" 이렇게 반환됨
+
+@ 예제 3
+
+enum UserRole {
+  admin,
+  manger
 }
-const myFordCar = new Ford();
-// myFordCar. 하게되면 Car와 Ford 안에 있는 메소드나 프로퍼티 전부 사용 가능
 
- - 변경 전()
-  class Ford1 implements Car {
-
-  }
-  // 'Ford1' 형식에 'Car' 형식의 mileage, price, color, drive, brake 속성이 없습니다. 라고 뜸.
-  // implements는 상속을 하는 것이 아닌 이 모양을 정의를 해주는 것.
-  // 에러를 해결하기 위해서는 Ford1 클래스에도 Car 클래스에 있는 속성들을 넣어주면 됨
-
-
- - 변경 후
-interface Part {
-  seats: number;
-  tire: number;
-}
-
-class Ford1 implements Car, Part {
-    mileage = 1;
-    price = 2;
-    color = 'white';
-    seats = 2;
-    tire = 3;
-
-    drive() {
-    return 'drive!';
-    }
-
-    brake() {
-    return 'brake!';
-    }
-}
-const myFordCar1 = new Ford1();
-// 위와같이 작성하면 완료. implements에는 interface도 넣어줄수 있는데 넣어줄 경우, 그 안의 속성도
-// 같이 넣어줘야함.
-
+type UserRoleKeys = keyof typeof UserRole
+// typeof 먼저 하고 keyof 해주면 enum도 키를 추출하고 Union 타입으로 반환함
